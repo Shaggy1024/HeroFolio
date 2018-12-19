ce765445b5a9be9074f65db67468c8fd4c2d2f6b
Shaggy1024 <38292839+Shaggy1024@users.noreply.github.com>
2018-12-19 14:45:21 -0700

    Update MainWindow.xaml.cs

--- HeroFolio/MainWindow.xaml.cs
+++ HeroFolio/MainWindow.xaml.cs
@@ -42,44 +42,122 @@ namespace HeroFolio
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            int strAbility1;
-            int strAbilityMod1=0;
+            int strAbility1, dexAbility1, conAbility1, intAbility1, wisAbility1, chaAbility1;
+            int strAbilityMod1=0, dexAbilityMod1=0, conAbilityMod1 = 0, intAbilityMod1 = 0, wisAbilityMod1 = 0, chaAbilityMod1 = 0;
 
             strAbility1 = int.Parse(strAbility.Text);
-            if (strAbility1 == 10 || strAbility1 == 11)
+            dexAbility1 = int.Parse(dexAbility.Text);
+            conAbility1 = int.Parse(conAbility.Text);
+            intAbility1 = int.Parse(intAbility.Text);
+            wisAbility1 = int.Parse(wisAbility.Text);
+            chaAbility1 = int.Parse(chaAbility.Text);
+
+            if (strAbility1 >= 10)
             {
-                strAbilityMod1 = 1;
-                strAbilityMod.Text = strAbilityMod1.ToString();
+                strAbilityMod1 = greaterThan(strAbility1);
             }
-            else if (strAbility1 == 12 || strAbility1 == 13)
+            else
             {
-                strAbilityMod1 = 2;
-                strAbilityMod.Text = strAbilityMod1.ToString();
+                strAbilityMod1 = lessThan(strAbility1);
             }
-            else if (strAbility1 == 14 || strAbility1 == 15)
+            if (dexAbility1 >= 10)
             {
-                strAbilityMod1 = 3;
-                strAbilityMod.Text = strAbilityMod1.ToString();
+                dexAbilityMod1 = greaterThan(dexAbility1);
             }
-            else if (strAbility1 == 16 || strAbility1 == 17)
+            else
             {
-                strAbilityMod1 = 4;
-                strAbilityMod.Text = strAbilityMod1.ToString();
+                dexAbilityMod1 = lessThan(dexAbility1);
             }
-            else if (strAbility1 == 18 || strAbility1 == 19)
+            if (conAbility1 >= 10)
             {
-                strAbilityMod1 = 5;
-                strAbilityMod.Text = strAbilityMod1.ToString();
+                conAbilityMod1 = greaterThan(conAbility1);
             }
+            else
+            {
+                conAbilityMod1 = lessThan(conAbility1);
+            }
+            if (intAbility1 >= 10)
+            {
+                intAbilityMod1 = greaterThan(intAbility1);
+            }
+            else
+            {
+                intAbilityMod1 = lessThan(intAbility1);
+            }
+            if (wisAbility1 >= 10)
+            {
+                wisAbilityMod1 = greaterThan(wisAbility1);
+            }
+            else
+            {
+                wisAbilityMod1 = lessThan(wisAbility1);
+            }
+            if (chaAbility1 >= 10)
+            {
+                chaAbilityMod1 = greaterThan(chaAbility1);
+            }
+            else
+            {
+                chaAbilityMod1 = lessThan(chaAbility1);
+            }
+            strAbilityMod.Text = strAbilityMod1.ToString();
+            dexAbilityMod.Text = dexAbilityMod1.ToString();
+            conAbilityMod.Text = conAbilityMod1.ToString();
+            intAbilityMod.Text = intAbilityMod1.ToString();
+            wisAbilityMod.Text = wisAbilityMod1.ToString();
+            chaAbilityMod.Text = chaAbilityMod1.ToString();
             AbilityEdit.Visibility=Visibility.Hidden;
             strAbility.IsReadOnly = true;
+            dexAbility.IsReadOnly = true;
+            conAbility.IsReadOnly = true;
+            intAbility.IsReadOnly = true;
+            wisAbility.IsReadOnly = true;
+            chaAbility.IsReadOnly = true;
             return;
+        }
+
+        private int greaterThan(int ability)
+        {
+            int mod = 0;
+            ability = ability - 10;
+            //accounts for ability being odd(id. 11 or 13 ect.)
+            if (ability % 2 == 0)
+                ability = ability;
+            else
+                ability = ability - 1;
+
+            for(int i=0; i < ability; i += 2)
+            {
+                mod++;
+            }
+            return mod;
+        }
+
+        private int lessThan(int ability)
+        {
+            int mod = 0;
+            //accounts for ability being odd(id. 9 or 7 ect.)
+            if (ability % 2 == 0)
+                ability = ability;
+            else
+                ability = ability - 1;
+
+            for(int i = 10; i < ability; i -= 2)
+            {
+                mod--;
+            }
+            return mod;
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
             AbilityEdit.Visibility = Visibility.Visible;
             strAbility.IsReadOnly = false;
+            dexAbility.IsReadOnly = false;
+            conAbility.IsReadOnly = false;
+            intAbility.IsReadOnly = false;
+            wisAbility.IsReadOnly = false;
+            chaAbility.IsReadOnly = false;
         }
     }
 }
